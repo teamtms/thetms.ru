73d73528dcd1ef49c99be18aec9e4ce55f536a85
iFB24M <fakem1ner@yandex.ru>
2024-01-03 20:56:01 +0400

    add suspense add vercel.json with redirects

--- src/Layout.component.tsx
+++ src/Layout.component.tsx
@@ -1,4 +1,5 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Suspense } from 'react'
 import { Outlet } from 'react-router-dom'
 
 const queryClient = new QueryClient()
@@ -6,10 +7,12 @@ const queryClient = new QueryClient()
 export const Layout = () => {
 	return (
 		<QueryClientProvider client={queryClient}>
-			ТМС сайт
-			<main className="main">
-				<Outlet />
-			</main>
+			<Suspense fallback={<>Загрузка необходимых для страницы компонентов...</>}>
+				ТМС сайт
+				<main className="main">
+					<Outlet />
+				</main>
+			</Suspense>
 		</QueryClientProvider>
 	)
 }
