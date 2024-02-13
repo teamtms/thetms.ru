b0d9cfd862ac0393aaf73b966b819123cda9c0e4
iFB24M <fakem1ner@yandex.ru>
2024-02-13 22:39:09 +0400

    fix linter errors

--- src/Layout.component.tsx
+++ src/Layout.component.tsx
@@ -1,12 +1,6 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { Suspense } from 'react'
 import { Outlet } from 'react-router-dom'
 import './Layout.scss'
-import { FluentProvider, Spinner } from '@fluentui/react-components'
-import { tmsDarkTheme, tmsLightTheme } from './themes/tmsTheme'
 import { Header } from './components/Header/Header.component'
-
-const queryClient = new QueryClient()
 
 export const Layout = () => {
 	return (
